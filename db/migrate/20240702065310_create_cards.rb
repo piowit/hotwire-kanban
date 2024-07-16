20dadfd27bd5fb7f44b95aea9de52895b11b743a
maikhel <mlecicki@gmail.com>
2024-07-16 20:47:56 +0200

    Add migrations with models

---
+++ db/migrate/20240702065310_create_cards.rb
@@ -0,0 +1,12 @@
+class CreateCards < ActiveRecord::Migration[7.1]
+  def change
+    create_table :cards do |t|
+      t.string :title
+      t.text :description
+      t.integer :position
+      t.references :board_column
+
+      t.timestamps
+    end
+  end
+end
